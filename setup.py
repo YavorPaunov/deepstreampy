389f7ae06dbcac4b6be5f7ecf86134f402ae4f29
Yavor Paunov <contact@yavorpaunov.com>
2016-04-21 21:55:24 +0200

    Add setup file and requirements.txt

---
+++ setup.py
@@ -0,0 +1,17 @@
+import os
+from setuptools import setup
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(name='deepstreampy',
+      version='0.1.1',
+      author='Yavor Paunov',
+      author_email='contact@yavorpaunov.com',
+      description='A deepstream.io client.',
+      license='MIT',
+      url='https://www.github.com/YavorPaunov/deepstreampy',
+      packages=['deepstreampy', 'tests', 'features'],
+      long_description=read('README.md'),
+      test_suite='tests')
