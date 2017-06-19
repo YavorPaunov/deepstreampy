6251c832b75336d957c8a0b997569396b5779b36
Charles Chan <charleswhchan@users.noreply.github.com>
2017-06-19 11:57:32 -0700

    Fix issue #1

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
+def requirements(filename='requirements.txt'):
+    with open(filename) as f:
+        requirements = f.read().splitlines()
+        return requirements
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -12,6 +16,7 @@ setup(name='deepstreampy',
       description='A deepstream.io client.',
       license='MIT',
       url='https://www.github.com/YavorPaunov/deepstreampy',
-      packages=['deepstreampy', 'tests', 'features'],
+      packages=find_packages(),
+      install_requires=requirements(),
       long_description=read('README.md'),
       test_suite='tests')
